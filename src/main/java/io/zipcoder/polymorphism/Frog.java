18894175d6d2f1f6798e1a2014640939166998d5
Stefun <hawkd215@gmail.com>
2019-07-07 01:24:07 -0400

    Pets are talking

---
+++ src/main/java/io/zipcoder/polymorphism/Frog.java
@@ -0,0 +1,14 @@
+package io.zipcoder.polymorphism;
+
+public class Frog extends Pet {
+
+    public Frog(String name){
+        super(name);
+    }
+
+
+    @Override
+    public String speak(){
+        return "Ribbit...Ribbit";
+    }
+}
--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,7 +1,78 @@
 package io.zipcoder.polymorphism;
 
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by leon on 11/6/17.
  */
 public class MainApplication {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Integer numberOfPets;
+
+        ArrayList<Pet> petList = new ArrayList<>();
+
+
+        System.out.println("How many pets do you have?" + "\nEnter 0 to quit");
+        while(!scanner.hasNextInt()){
+            System.out.println("That is not a valid number of pets");
+            scanner.next();
+        }
+
+        numberOfPets = scanner.nextInt();
+
+        if (numberOfPets < 0){
+            System.out.println("Looks like you have no pets...Great decision!" + "\nIf you do in fact have pets, restart and enter a positive number.");
+        }
+
+        while (numberOfPets > 0){
+            while(!scanner.hasNextInt()){
+                System.out.println("That is not a valid number of pets.");
+                scanner.next();
+            }
+
+            for(int i = 0; i < numberOfPets; i++) {
+                askForPetType();
+                String petType = scanner.next().toLowerCase();
+
+                if (petType.equals("cat")) {
+                    askForPetName();
+                    String petName = scanner.next();
+                    petList.add(new Cat(petName));
+                    numberOfPets--;
+
+
+                } else if (petType.equals("dog")) {
+                    askForPetName();
+                    String petName = scanner.next();
+                    petList.add(new Dog(petName));
+                    numberOfPets--;
+                } else if (petType.equals("fox")) {
+                    askForPetName();
+                    String petName = scanner.next();
+                    petList.add(new Frog(petName));
+                    numberOfPets--;
+                } else {
+                    System.out.println("Please enter a valid pet type.");
+                    i--;
+                }
+            }
+    }
+        for(int i = 0; i<petList.size(); i++){
+            System.out.println("The animal named " +petList.get(i).getName() + " says " + petList.get(i).speak() + "!");
+        }
+
+
+    }
+    private static void askForPetType() {
+        System.out.println("Is the pet a cat, dog, or frog?");
+    }
+    private static void askForPetName() {
+        System.out.println("What is the name of that pet");
+    }
 }
+
